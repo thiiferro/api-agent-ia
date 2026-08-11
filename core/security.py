1f4338150b3874571275cbe004d25068e99684af
Thiago Ferro <thiagoferro260503@gmail.coim>
2026-08-11 19:37:09 -0300

    Adicionando funções de criação de hash e verificação de senha

--- core/security.py
+++ core/security.py
@@ -2,3 +2,19 @@ from datetime import datetime, timedelta, timezone
 
 import jwt
 from pwdlib import PasswordHash
+
+from core.config import settings
+
+password_hash = PasswordHash.recommended()
+
+
+def hash_password(password: str) -> str:
+    return password_hash.hash(password)
+
+
+def verify_password(
+    password: str,
+    hashed_password: str
+) -> bool:
+    return password_hash.verify(password, hashed_password)
+
